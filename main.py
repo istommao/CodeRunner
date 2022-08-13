23b634a2ed68df8c08a80160c74143a32efd00b9
CodeMax <CodeMax@web3mq.com>
2022-08-13 21:05:32 +0800

    Add cors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
 
@@ -9,10 +10,23 @@ from core.docker_client import DockerClient
 
 
 app = FastAPI()
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 class CodeRunForm(BaseModel):
     language: str
+    version: Optional[str] = "0.01"
     code: str
     timestamp: int
 
